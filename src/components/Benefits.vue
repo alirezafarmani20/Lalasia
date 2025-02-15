12f5725a56cfc4efe19f5813cb56d024e8c98669
alireza <farmanialireza668@gmail.com>
2025-02-15 15:33:54 +0330

    create a part of bebefits components

---
+++ src/components/Benefits.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Container from '@/layouts/Container.vue';
+</script>
+
+<template>
+    <div class="w-full">
+        <Container>
+            <div class="w-full">
+                <div class="flex items-center justify-between">
+                    <div class="flex flex-col gap-3 w-full sm:w-[321px]">
+                        <span class="text-secondary-1 capitalize text-[16px] sm:text-[18px] font-medium">benefits</span>
+                        <h2 class="text-wrap text-title-1 font-bold text-[20px] sm:text-[30px]">benefits when useing our
+                            service</h2>
+                    </div>
+                    <div class="dlex justify-between w-full sm:w-[321px]">
+                        <p class="text-wrap text-title-2 font-medium text-[15px]">Lorem ipsum, dolor sit amet
+                            consectetur adipisicing elit. Cum at iusto modi numquam sint. Eligendi laboriosam nihil
+                            repudiandae.</p>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center">
+                    
+                </div>
+            </div>
+        </Container>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
 import MainBaner from "@/components/MainBaner.vue";
+import Benefits from "@/components/Benefits.vue";
 </script>
 
 <template>
   <div>
     <MainBaner />
-    <h1 class="text-red-600 text-3xl font-bold underline">hello this is home page</h1>
+  </div>
+  <div>
+    <Benefits />
   </div>
 </template>
