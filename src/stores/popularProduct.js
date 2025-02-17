56f6791df3ecf98b105e3c27d218596823168d81
alireza <farmanialireza668@gmail.com>
2025-02-17 14:49:13 +0330

    format the code

--- src/stores/popularProduct.js
+++ src/stores/popularProduct.js
@@ -2,31 +2,31 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import { baseUrl } from "./base";
 
-export const usePopularProductStore = defineStore('usePopularProduct', {
-    state: () => ({
-        data: [],
-        loading: true,
-    }),
+export const usePopularProductStore = defineStore("usePopularProduct", {
+  state: () => ({
+    data: [],
+    loading: true,
+  }),
 
-    getters: () => {
-        showData: (state) => {
-            return state.data
-        }
-    },
+  getters: () => {
+    showData: (state) => {
+      return state.data;
+    };
+  },
 
-    actions: {
-        async getData() {
-            // get data
-            try {
-                const response = await axios.get(`${baseUrl}popular-product`).then((res) => {
-                    this.data = res.data;
-                    console.log(res.data);
-                    this.loading = false;
-                });
-            } catch (err) {
-                console.log(`this is a error: ${err.message}`);
-                this.loading = true;
-            }
-        },
+  actions: {
+    async getData() {
+      // get data
+      try {
+        const response = await axios.get(`${baseUrl}popular-product`).then((res) => {
+          this.data = res.data;
+          console.log(res.data);
+          this.loading = false;
+        });
+      } catch (err) {
+        console.log(`this is a error: ${err.message}`);
+        this.loading = true;
+      }
     },
-})
+  },
+});
